b36c8214bbd18de3081a0b4047bede9d957bb0e3
Pontus Liljekvist <alex.p.liljekvist@gmail.com>
2024-07-17 13:55:07 +0200

    renaming and moving

---
+++ Assets/Code/Scripts/PlayerHUD.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUD : MonoBehaviour
+{
+
+    [SerializeField] private Image _heart1;
+    [SerializeField] private Image _heart2;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
